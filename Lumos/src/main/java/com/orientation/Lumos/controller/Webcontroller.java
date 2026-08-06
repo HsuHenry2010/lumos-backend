e96ba3e8ca5713369394bcf256b45da082d7b58e
HsuHenry2010 <hsuhenry2010@gmail.com>
2026-08-06 08:13:28 +0800

    更新三校聯合迎新標題、新增地圖與記分板頁面
    步驟四：執行 Commit and Push

--- Lumos/src/main/java/com/orientation/Lumos/controller/Webcontroller.java
+++ Lumos/src/main/java/com/orientation/Lumos/controller/Webcontroller.java
@@ -6,9 +6,39 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class Webcontroller {
 
+    // 首頁
     @GetMapping("/")
     public String index() {
-        // 這會對應去找 templates/index.html
         return "index";
+    }
+
+    // 活動介紹
+    @GetMapping("/activities")
+    public String activities() {
+        return "activities";
+    }
+
+    // 校園地圖
+    @GetMapping("/map")
+    public String map() {
+        return "map";
+    }
+
+    // 學長姐
+    @GetMapping("/seniors")
+    public String seniors() {
+        return "seniors";
+    }
+
+    // 記分板
+    @GetMapping("/scoreboard")
+    public String scoreboard() {
+        return "scoreboard";
+    }
+
+    // 大地遊戲
+    @GetMapping("/games")
+    public String games() {
+        return "games";
     }
 }
